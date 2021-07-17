cd13c09573e76cf9e15e6a9a3ddcaf2b4f2be8e1
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-07-17 16:53:12 -0700

    Added circle element. A circle.

---
+++ src/elements.py
@@ -0,0 +1,55 @@
+#
+#  CS Animation
+#  A tool for creating computer science explanatory videos.
+#  Copyright Patrick Huang 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import numpy as np
+from typing import Tuple
+from .props import *
+from . import lib
+
+
+class Element:
+    show: BoolProp
+
+    def __init__(self) -> None:
+        self.show = BoolProp(True)
+
+    def render(self, img: np.ndarray, frame: float) -> None:
+        ...
+
+
+class Circle(Element):
+    color: VectorProp
+    center: VectorProp
+    radius: FloatProp
+    border: FloatProp
+
+    def __init__(self, color: Tuple[float, ...] = (255, 255, 255, 255), center: Tuple[float, float] = (0, 0),
+            radius: float = 100, border: float = 0) -> None:
+        super().__init__()
+        self.color = VectorProp(FloatProp, 4, color)
+        self.center = VectorProp(FloatProp, 2, center)
+        self.radius = FloatProp(radius)
+        self.border = FloatProp(border)
+
+    def render(self, img: np.ndarray, frame: float) -> None:
+        color = self.color.value(frame)
+        center = self.center.value(frame)
+        radius = self.radius.value(frame)
+        border = self.border.value(frame)
+        lib.draw.circle(img, color, center, radius, border)
