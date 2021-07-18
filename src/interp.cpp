01554aa6bc2cefc931dc36167c4ba183ebcdb1b7
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-07-18 14:08:05 -0700

    Fixed sine interpolation bugs.

--- src/interp.cpp
+++ src/interp.cpp
@@ -19,9 +19,26 @@
 
 #define  PI  3.1415926535
 
+#include <iostream>
 #include <cmath>
 
 typedef  const double  CD;
+
+
+double range(CD old_min, CD old_max, CD new_min, CD new_max, CD value) {
+    CD fac = (value-old_min) / (old_max-old_min);
+    CD new_v = fac*(new_max-new_min) + new_min;
+    return new_v;
+}
+
+
+double sin(CD x_min, CD x_max, CD y_min, CD y_max, CD fac) {
+    CD fac_x = fmod(x_min + fac*(x_max-x_min), 1.);
+    CD real_x = range(0, 1, 0, PI*2, fac_x);
+    CD output = std::sin(real_x);
+    CD out_y = range(-1, 1, y_min, y_max, output);
+    return out_y;
+}
 
 
 extern "C" double linear(CD f1, CD f2, CD v1, CD v2, CD frame) {
@@ -31,6 +48,5 @@ extern "C" double linear(CD f1, CD f2, CD v1, CD v2, CD frame) {
 
 extern "C" double sine(CD f1, CD f2, CD v1, CD v2, CD frame) {
     CD fac = (frame-f1) / (f2-f1);
-    CD new_fac = std::sin((PI*fac)-(PI/2));
-    return v1 + (v2-v1)*new_fac;
+    return sin(-0.25, 0.25, v1, v2, fac);
 }
