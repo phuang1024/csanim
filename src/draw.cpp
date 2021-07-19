e03ced10c49f1fd4edb50a4fbe4f4cccfc2357e9
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-07-19 14:28:14 -0700

    Documentation for draw functions.

--- src/draw.cpp
+++ src/draw.cpp
@@ -17,6 +17,12 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+/*
+Functions for drawing antialiased shapes.
+Takes in a numpy array as an image with shape (height, width, 3)
+img variables are raveled numpy arrays.
+*/
+
 #include <iostream>
 #include <cmath>
 
@@ -29,20 +35,54 @@ typedef  const double   CD;
 
 
 void set(UCH* img, const UINT width, const UINT x, const UINT y, const UCH channel, const UCH value) {
+    /*
+    Sets pixel and channel of image to a value.
+    :param img: Image.
+    :param width: Image width.
+    :param x: X coordinate.
+    :param y: Y coordinate.
+    :param channel: Channel number, corresponding to B, G, R.
+    :param value: Number from 0 to 255.
+    */
     img[3*(y*width + x) + channel] = value;
 }
 
 void setc(UCH* img, const UINT width, const UINT x, const UINT y, const UCH r, const UCH g, const UCH b) {
+    /*
+    Sets pixel to color. Equivalent to three calls of set()
+    :param img: Image.
+    :param width: Image width.
+    :param x: X coordinate.
+    :param y: Y coordinate.
+    :param r, g, b: R, G, B values.
+    */
     set(img, width, x, y, 0, r);
     set(img, width, x, y, 1, g);
     set(img, width, x, y, 2, b);
 }
 
 void get(UCH* img, const UINT width, const UINT x, const UINT y, const UCH channel, UCH* value) {
+    /*
+    Gets value at pixel and channel and modifies "value" param.
+    :param img: Image.
+    :param width: Image width.
+    :param x: X coordinate.
+    :param y: Y coordinate.
+    :param channel: Channel number, corresponding to B, G, R.
+    :param value: Value pointer. Will be modified to be the obtained value.
+    */
     value[0] = img[3*(y*width + x) + channel];
 }
 
 void getc(UCH* img, const UINT width, const UINT x, const UINT y, UCH* color) {
+    /*
+    Gets value at pixel and modifies "value" param. Equivalent to 3 calls of get()
+    :param img: Image.
+    :param width: Image width.
+    :param x: X coordinate.
+    :param y: Y coordinate.
+    :param value: Value pointer. Will be modified to be the obtained value.
+    */
     get(img, width, x, y, 0, color+0);
     get(img, width, x, y, 1, color+1);
     get(img, width, x, y, 2, color+2);
@@ -50,25 +90,60 @@ void getc(UCH* img, const UINT width, const UINT x, const UINT y, UCH* color) {
 
 
 void mix(UCH* dest, const UCH* c1, const UCH* c2, CD fac) {
+    /*
+    Mixes two colors with a factor.
+    :param dest: Destination array. Will be modified.
+    :param c1: Color 1.
+    :param c2: Color 2.
+    :param fac: Factor. 0 = full c1, 1 = full c2
+    */
     for (int i = 0; i < 3; i++)
         dest[i] = c1[i]*(1-fac) + c2[i]*fac;
 }
 
 double pythag(CD dx, CD dy) {
+    /*
+    Pythagorean distance.
+    :param dx: X delta.
+    :param dy: Y delta.
+    */
     return std::pow((dx*dx) + (dy*dy), 0.5);
 }
 
 int ibounds(const int v, const int vmin = 0, const int vmax = 1) {
+    /*
+    Integer bounds.
+    :param v: Value.
+    :param vmin: Minimum value.
+    :param vmax: Maximum value.
+    */
     return min(max(v, vmin), vmax);
 }
 
 double dbounds(CD v, CD vmin = 0, CD vmax = 1) {
+    /*
+    Double bounds.
+    :param v: Value.
+    :param vmin: Minimum value.
+    :param vmax: Maximum value.
+    */
     return min(max(v, vmin), vmax);
 }
 
 
 extern "C" void circle(UCH* img, const UINT width, const UINT height, CD cx, CD cy,
         CD rad, CD border, CD r, CD g, CD b, CD a) {
+    /*
+    Draws a circle.
+    :param img: Image.
+    :param width: Image width.
+    :param height: Image height.
+    :param cx: Center X.
+    :param cy: Center Y.
+    :param rad: Radius.
+    :param border: Border thickness. Set to 0 for filled.
+    :param r, g, b, a: R, G, B, A values.
+    */
     const int xmin = max((int)(cx-rad-1), 0);
     const int xmax = min((int)(cx+rad+1), (int)width-1);
     const int ymin = max((int)(cy-rad-1), 0);
@@ -95,6 +170,22 @@ extern "C" void circle(UCH* img, const UINT width, const UINT height, CD cx, CD 
 
 extern "C" void rect(UCH* img, const UINT width, const UINT height, CD dx, CD dy, CD dw, CD dh,
         CD border, CD border_rad, CD tl_rad, CD tr_rad, CD bl_rad, CD br_rad, CD r, CD g, CD b, CD a) {
+    /*
+    Draws a rectangle.
+    :param img: Image.
+    :param width: Image width.
+    :param height: Image height.
+    :param dx: Top left X.
+    :param dy: Top left Y.
+    :param dw: Width.
+    :param dh: Height.
+    :param border: Border thickness. Set to 0 for filled.
+    :param border_rad: Radius of corner rounding.
+    :param tl_rad: Top left corner radius.
+    :param tr_rad: Top right corner radius.
+    :param bl_rad: Bottom left corner radius.
+    :param br_rad: Bottom right corner radius.
+    */
     CD radii[4] = {
         (tl_rad < 0) ? border_rad : tl_rad,
         (tr_rad < 0) ? border_rad : tr_rad,
