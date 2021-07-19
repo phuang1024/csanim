6d7ecc01527d64a52ebe656f4dc32ffa279bc0dd
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-07-19 14:52:35 -0700

    Docstrings for interpolation.

--- src/interp.cpp
+++ src/interp.cpp
@@ -17,6 +17,10 @@
 //  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+/*
+Interpolation between two keyframes.
+*/
+
 #define  PI  3.1415926535
 
 #include <iostream>
@@ -26,6 +30,14 @@ typedef  const double  CD;
 
 
 double range(CD old_min, CD old_max, CD new_min, CD new_max, CD value) {
+    /*
+    Maps a value from an old range to a new range.
+    :param old_min: Old minimum.
+    :param old_max: Old maximum.
+    :param new_min: New minimum.
+    :param new_max: New maximum.
+    :param value: Value to map.
+    */
     CD fac = (value-old_min) / (old_max-old_min);
     CD new_v = fac*(new_max-new_min) + new_min;
     return new_v;
@@ -33,6 +45,14 @@ double range(CD old_min, CD old_max, CD new_min, CD new_max, CD value) {
 
 
 double sin(CD x_min, CD x_max, CD y_min, CD y_max, CD fac) {
+    /*
+    A easier-to-use sine function.
+    :param x_min: X minimum.
+    :param x_max: X maximum.
+    :param y_min: Output minimum.
+    :param y_max: Output maximum.
+    :param fac: Input factor. Mapped to x_min, x_max.
+    */
     CD fac_x = fmod(x_min + fac*(x_max-x_min), 1.);
     CD real_x = range(0, 1, 0, PI*2, fac_x);
     CD output = std::sin(real_x);
@@ -42,11 +62,18 @@ double sin(CD x_min, CD x_max, CD y_min, CD y_max, CD fac) {
 
 
 extern "C" double linear(CD f1, CD f2, CD v1, CD v2, CD frame) {
+    /*
+    Linear interpolation between two keyframes.
+    */
     CD fac = (frame-f1) / (f2-f1);
     return v1 + (v2-v1)*fac;
 }
 
 extern "C" double sine(CD f1, CD f2, CD v1, CD v2, CD frame) {
+    /*
+    Sine interpolation between two keyframes.
+    Uses the section with non-negative derivative.
+    */
     CD fac = (frame-f1) / (f2-f1);
     return sin(-0.25, 0.25, v1, v2, fac);
 }
